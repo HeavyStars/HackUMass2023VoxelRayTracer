1f5e57268c6cbbafaeb6ed93ae58eed4372b33fe
Heavy Stars <jwerhane1@gmail.com>
2023-11-12 00:13:29 -0500

    made renderer

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,1 +1,35 @@
 //renderer.cpp
+#include "renderer.hpp"
+
+Renderer::Renderer(int width, int height) : 
+_marchDrawable(sf::Quads, 4) {
+  sf::Vector2f uResolution(width, height);
+  _context.create(width, height);
+  _context.setSmooth(false);
+  _width = width;
+  _height = height;
+
+  _marchDrawable[0].position = sf::Vector2f(0, 0);
+  _marchDrawable[1].position = sf::Vector2f(uResolution.x, 0);
+  _marchDrawable[2].position = sf::Vector2f(uResolution.x, uResolution.y);
+  _marchDrawable[3].position = sf::Vector2f(0, uResolution.y);
+  _marchDrawable[0].color = sf::Color(0, 0, 250, 255);
+  _marchDrawable[1].color = sf::Color(80, 24, 100, 255);
+  _marchDrawable[2].color = sf::Color(0, 80, 80, 255);
+  _marchDrawable[3].color = sf::Color(180, 0, 75, 255);
+}
+
+Renderer::~Renderer(){
+
+}
+
+void Renderer::update(){
+
+}
+
+const sf::Texture & Renderer::render(){
+  _context.clear();
+  _context.draw(_marchDrawable);
+  _context.display();
+  return _context.getTexture();
+}
